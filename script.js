ca18cd396d1ca973293a0237cf74da90ad033cd9
mertkiyar <mertkiyar69@gmail.com>
2026-04-19 12:56:04 +0300

    feat: add ball with new dimensions at the center

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
 const canvas = document.getElementById("board");
 const ctx = canvas.getContext("2d");
 
+canvas.height = 600;
+canvas.width = 600;
+
 const ball = {
     x: 100,
     y: 100,
-    width: 10,
-    height: 10,
+    width: 20,
+    height: 20,
     vx: 0,
     vy: 0
 }
 
 function update() {
 
-
 }
 
 function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+    ctx.fillStyle = "white";
+    ctx.fillRect(canvas.width / 2 - ball.height / 2, canvas.height / 2 - ball.height / 2, ball.width, ball.height);
 }
 
 function gameLoop() {
@@ -25,3 +30,5 @@ function gameLoop() {
 
     requestAnimationFrame(gameLoop);
 }
+
+gameLoop();
