9612e29127d686ec62c9f55cf084fc4d36f1f17d
mertkiyar <mertkiyar69@gmail.com>
2026-04-19 14:04:56 +0300

    feat: add base game logic and scoretext at the middle of canvas

--- script.js
+++ script.js
@@ -4,27 +4,35 @@ const ctx = canvas.getContext("2d");
 canvas.height = 600;
 canvas.width = 600;
 
+let score = 10;
+let levelDone = false;
 const ball = {
     x: 0,
     y: 0,
     width: 20,
     height: 20,
-    vx: 9,
+    vx: 4,
     vy: 5
 }
+
 ball.x = canvas.width / 2 - ball.width / 2; // center on x axis 
 ball.y = canvas.height / 2 - ball.height / 2; // center on y axis
 
 function update() {
     ball.x += ball.vx;
     ball.y += ball.vy;
-
     if (ball.x + ball.width > canvas.width || ball.x < 0) {
         ball.vx *= -1;
+        score -= 1;
     }
 
     if (ball.y + ball.width > canvas.height || ball.y < 0) {
         ball.vy *= -1
+        score -= 1;
+    }
+
+    if (score <= 0) {
+        levelDone = true;
     }
 }
 
@@ -33,9 +41,18 @@ function draw() {
 
     ctx.fillStyle = "white";
     ctx.fillRect(ball.x, ball.y, ball.width, ball.height);
+
+    ctx.font = "190px Arial";
+    ctx.fillStyle = "white";
+    ctx.textAlign = "center"; // center horizontal
+    ctx.textBaseline = "middle" // center vertical
+    ctx.globalAlpha = "0.3" // opacity
+    ctx.fillText(score, canvas.width / 2, canvas.height / 2);
+    ctx.globalAlpha = 1; // reset opacity for ball
 }
 
 function gameLoop() {
+    if (levelDone) return;
     update();
     draw();
 
