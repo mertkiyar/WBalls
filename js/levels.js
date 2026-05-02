c21a17124d39d46f50ba4a2d885d0a2a00ebfd17
mertkiyar <mertkiyar69@gmail.com>
2026-05-02 17:04:31 +0300

    fix: update level designs due to some easy solutions

--- js/levels.js
+++ js/levels.js
@@ -10,8 +10,9 @@ export const LEVELS = [
         id: 2,
         score: 7,
         holes: [
-            { side: "left", start: 100, end: 300, color: null, closesAt: null },
-            { side: "right", start: 300, end: 500, color: null, closesAt: null }
+            { side: "left", start: 300, end: 500, color: null, closesAt: null },
+            { side: "bottom", start: 300, end: 500, color: null, closesAt: null },
+            { side: "right", start: 0, end: 100, color: null, closesAt: null }
         ]
     },
     {
@@ -26,10 +27,13 @@ export const LEVELS = [
         id: 4,
         score: 12,
         holes: [
-            { side: "top", start: 0, end: 200, color: null, closesAt: null },
-            { side: "bottom", start: 400, end: 600, color: null, closesAt: null },
-            { side: "left", start: 200, end: 400, color: null, closesAt: null },
-            { side: "right", start: 200, end: 400, color: null, closesAt: null }
+            { side: "top", start: 50, end: 200, color: null, closesAt: null },
+            { side: "bottom", start: 500, end: 600, color: null, closesAt: null },
+            { side: "left", start: 300, end: 400, color: null, closesAt: null },
+            { side: "right", start: 200, end: 400, color: null, closesAt: null },
+
+            { side: "left", start: 500, end: 600, color: "orange", closesAt: 8 },
+            { side: "bottom", start: 0, end: 100, color: "orange", closesAt: 8 }
         ]
     },
     {
@@ -50,6 +54,8 @@ export const LEVELS = [
             { side: "top", start: 450, end: 600, color: null, closesAt: null },
             { side: "bottom", start: 0, end: 150, color: null, closesAt: null },
             { side: "bottom", start: 450, end: 600, color: null, closesAt: null },
+            { side: "top", start: 250, end: 350, color: "dodgerblue", closesAt: 8 },
+            { side: "bottom", start: 250, end: 350, color: "dodgerblue", closesAt: 8 },
             { side: "left", start: 250, end: 350, color: "dodgerblue", closesAt: 8 },
             { side: "right", start: 250, end: 350, color: "dodgerblue", closesAt: 8 }
         ]
@@ -58,8 +64,10 @@ export const LEVELS = [
         id: 7,
         score: 19,
         holes: [
-            { side: "top", start: 0, end: 600, color: "crimson", closesAt: 9 },
-            { side: "left", start: 0, end: 600, color: "crimson", closesAt: 9 },
+            { side: "top", start: 0, end: 200, color: "dodgerblue", closesAt: 2 },
+            { side: "left", start: 0, end: 200, color: "dodgerblue", closesAt: 2 },
+            { side: "top", start: 200, end: 600, color: "crimson", closesAt: 9 },
+            { side: "left", start: 200, end: 600, color: "crimson", closesAt: 9 },
             { side: "right", start: 250, end: 350, color: null, closesAt: null },
             { side: "bottom", start: 250, end: 350, color: null, closesAt: null }
         ]
@@ -68,26 +76,21 @@ export const LEVELS = [
         id: 8,
         score: 21,
         holes: [
-            { side: "top", start: 100, end: 200, color: null, closesAt: null },
-            { side: "top", start: 400, end: 500, color: null, closesAt: null },
-            { side: "bottom", start: 100, end: 200, color: null, closesAt: null },
-            { side: "bottom", start: 400, end: 500, color: null, closesAt: null },
-            { side: "left", start: 100, end: 500, color: "gold", closesAt: 10 },
-            { side: "right", start: 100, end: 500, color: "gold", closesAt: 10 }
+
+            { side: "top", start: 300, end: 600, color: null, closesAt: null },
+            { side: "bottom", start: 100, end: 300, color: null, closesAt: null },
+            { side: "left", start: 100, end: 300, color: "gold", closesAt: 10 },
+            { side: "right", start: 300, end: 600, color: "gold", closesAt: 10 }
         ]
     },
     {
         id: 9,
         score: 23,
         holes: [
-            { side: "top", start: 0, end: 250, color: "purple", closesAt: 11 },
-            { side: "top", start: 350, end: 600, color: "purple", closesAt: 11 },
-            { side: "bottom", start: 0, end: 250, color: "purple", closesAt: 11 },
-            { side: "bottom", start: 350, end: 600, color: "purple", closesAt: 11 },
-            { side: "left", start: 0, end: 250, color: "purple", closesAt: 11 },
-            { side: "left", start: 350, end: 600, color: "purple", closesAt: 11 },
-            { side: "right", start: 0, end: 250, color: "purple", closesAt: 11 },
-            { side: "right", start: 350, end: 600, color: "purple", closesAt: 11 }
+            { side: "top", start: 0, end: 350, color: "purple", closesAt: 11 },
+            { side: "bottom", start: 250, end: 600, color: "purple", closesAt: 11 },
+            { side: "left", start: 250, end: 600, color: "purple", closesAt: 11 },
+            { side: "right", start: 0, end: 350, color: "purple", closesAt: 11 }
         ]
     },
     {
