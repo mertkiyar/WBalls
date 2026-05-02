d142fd7608f555dbc9c514b577303a6609ab7031
mertkiyar <mertkiyar69@gmail.com>
2026-05-02 15:54:22 +0300

    feat: add level and some holes

--- js/levels.js
+++ js/levels.js
@@ -3,93 +3,110 @@ export const LEVELS = [
         id: 1,
         score: 4,
         holes: [
-            { side: "top", start: 300, end: 500, color: null, closesAt: null },
-            { side: "left", start: 200, end: 450, color: null, closesAt: null },
-            { side: "right", start: 250, end: 550, color: null, closesAt: null }
+            { side: "top", start: 200, end: 400, color: null, closesAt: null }
         ]
     },
     {
         id: 2,
         score: 7,
         holes: [
-            { side: "top", start: 200, end: 500, color: null, closesAt: null },
-            { side: "left", start: 250, end: 450, color: null, closesAt: null },
-            { side: "right", start: 400, end: 550, color: null, closesAt: null },
-            { side: "bottom", start: 0, end: 200, color: null, closesAt: null },
+            { side: "left", start: 100, end: 300, color: null, closesAt: null },
+            { side: "right", start: 300, end: 500, color: null, closesAt: null }
         ]
     },
     {
         id: 3,
         score: 9,
         holes: [
-            { side: "top", start: 300, end: 500, color: null, closesAt: null },
-            { side: "left", start: 200, end: 450, color: null, closesAt: null },
-            { side: "right", start: 250, end: 550, color: null, closesAt: null }
+            { side: "bottom", start: 0, end: 600, color: "tomato", closesAt: 4 },
+            { side: "top", start: 250, end: 350, color: null, closesAt: null }
         ]
     },
     {
         id: 4,
         score: 12,
         holes: [
-            { side: "top", start: 300, end: 500, color: null, closesAt: null },
-            { side: "left", start: 200, end: 450, color: null, closesAt: null },
-            { side: "right", start: 250, end: 550, color: null, closesAt: null }
+            { side: "top", start: 0, end: 200, color: null, closesAt: null },
+            { side: "bottom", start: 400, end: 600, color: null, closesAt: null },
+            { side: "left", start: 200, end: 400, color: null, closesAt: null },
+            { side: "right", start: 200, end: 400, color: null, closesAt: null }
         ]
     },
     {
         id: 5,
         score: 15,
         holes: [
-            { side: "top", start: 300, end: 500, color: null, closesAt: null },
-            { side: "left", start: 200, end: 450, color: null, closesAt: null },
-            { side: "right", start: 250, end: 550, color: null, closesAt: null }
+            { side: "left", start: 0, end: 600, color: "orange", closesAt: 7 },
+            { side: "right", start: 0, end: 600, color: "orange", closesAt: 7 },
+            { side: "top", start: 100, end: 200, color: null, closesAt: null },
+            { side: "bottom", start: 400, end: 500, color: null, closesAt: null }
         ]
     },
     {
         id: 6,
         score: 17,
         holes: [
-            { side: "top", start: 300, end: 500, color: null, closesAt: null },
-            { side: "left", start: 200, end: 450, color: null, closesAt: null },
-            { side: "right", start: 250, end: 550, color: null, closesAt: null }
+            { side: "top", start: 0, end: 150, color: null, closesAt: null },
+            { side: "top", start: 450, end: 600, color: null, closesAt: null },
+            { side: "bottom", start: 0, end: 150, color: null, closesAt: null },
+            { side: "bottom", start: 450, end: 600, color: null, closesAt: null },
+            { side: "left", start: 250, end: 350, color: "dodgerblue", closesAt: 8 },
+            { side: "right", start: 250, end: 350, color: "dodgerblue", closesAt: 8 }
         ]
     },
     {
         id: 7,
         score: 19,
         holes: [
-            { side: "top", start: 300, end: 500, color: null, closesAt: null },
-            { side: "left", start: 200, end: 450, color: null, closesAt: null },
-            { side: "right", start: 250, end: 550, color: null, closesAt: null }
+            { side: "top", start: 0, end: 600, color: "crimson", closesAt: 9 },
+            { side: "left", start: 0, end: 600, color: "crimson", closesAt: 9 },
+            { side: "right", start: 250, end: 350, color: null, closesAt: null },
+            { side: "bottom", start: 250, end: 350, color: null, closesAt: null }
         ]
     },
     {
         id: 8,
         score: 21,
         holes: [
-            { side: "top", start: 300, end: 500, color: null, closesAt: null },
-            { side: "left", start: 200, end: 450, color: null, closesAt: null },
-            { side: "right", start: 250, end: 550, color: null, closesAt: null }
+            { side: "top", start: 100, end: 200, color: null, closesAt: null },
+            { side: "top", start: 400, end: 500, color: null, closesAt: null },
+            { side: "bottom", start: 100, end: 200, color: null, closesAt: null },
+            { side: "bottom", start: 400, end: 500, color: null, closesAt: null },
+            { side: "left", start: 100, end: 500, color: "gold", closesAt: 10 },
+            { side: "right", start: 100, end: 500, color: "gold", closesAt: 10 }
         ]
     },
     {
         id: 9,
         score: 23,
         holes: [
-            { side: "top", start: 300, end: 500, color: null, closesAt: null },
-            { side: "left", start: 200, end: 450, color: null, closesAt: null },
-            { side: "right", start: 250, end: 550, color: null, closesAt: null }
+            { side: "top", start: 0, end: 250, color: "purple", closesAt: 11 },
+            { side: "top", start: 350, end: 600, color: "purple", closesAt: 11 },
+            { side: "bottom", start: 0, end: 250, color: "purple", closesAt: 11 },
+            { side: "bottom", start: 350, end: 600, color: "purple", closesAt: 11 },
+            { side: "left", start: 0, end: 250, color: "purple", closesAt: 11 },
+            { side: "left", start: 350, end: 600, color: "purple", closesAt: 11 },
+            { side: "right", start: 0, end: 250, color: "purple", closesAt: 11 },
+            { side: "right", start: 350, end: 600, color: "purple", closesAt: 11 }
         ]
     },
     {
         id: 10,
         score: 26,
         holes: [
-            { side: "top", start: 300, end: 500, color: null, closesAt: null },
-            { side: "left", start: 200, end: 450, color: null, closesAt: null },
-            { side: "right", start: 250, end: 550, color: null, closesAt: null }
+            { side: "top", start: 150, end: 450, color: "red", closesAt: 15 },
+            { side: "bottom", start: 150, end: 450, color: "red", closesAt: 15 },
+            { side: "left", start: 150, end: 450, color: "red", closesAt: 15 },
+            { side: "right", start: 150, end: 450, color: "red", closesAt: 15 },
+
+            { side: "top", start: 0, end: 50, color: "hotpink", closesAt: 8 },
+            { side: "top", start: 550, end: 600, color: "hotpink", closesAt: 8 },
+            { side: "bottom", start: 0, end: 50, color: "hotpink", closesAt: 8 },
+            { side: "bottom", start: 550, end: 600, color: "hotpink", closesAt: 8 },
+            { side: "left", start: 0, end: 50, color: "hotpink", closesAt: 8 },
+            { side: "left", start: 550, end: 600, color: "hotpink", closesAt: 8 },
+            { side: "right", start: 0, end: 50, color: "hotpink", closesAt: 8 },
+            { side: "right", start: 550, end: 600, color: "hotpink", closesAt: 8 }
         ]
     }
-
-    // levels can be increased with the template
-]
+];
