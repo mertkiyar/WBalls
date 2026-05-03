c7ef86ab3bfb17d0c4945ee8db3e3b02ddcb628b
mertkiyar <mertkiyar69@gmail.com>
2026-05-04 00:03:33 +0300

    fix: update some levels structure and make playable

--- js/levels.js
+++ js/levels.js
@@ -64,43 +64,53 @@ export const LEVELS = [
         id: 7,
         score: 19,
         holes: [
-            { side: "top", start: 0, end: 200, color: "dodgerblue", closesAt: 2 },
-            { side: "left", start: 0, end: 200, color: "dodgerblue", closesAt: 2 },
-            { side: "top", start: 200, end: 600, color: "crimson", closesAt: 9 },
-            { side: "left", start: 200, end: 600, color: "crimson", closesAt: 9 },
+            { side: "top", start: 0, end: 100, color: "dodgerblue", closesAt: 2 },
+            { side: "left", start: 0, end: 100, color: "dodgerblue", closesAt: 2 },
+            { side: "top", start: 300, end: 600, color: "crimson", closesAt: 9 },
+            { side: "left", start: 300, end: 600, color: "crimson", closesAt: 9 },
             { side: "right", start: 250, end: 350, color: null, closesAt: null },
             { side: "bottom", start: 250, end: 350, color: null, closesAt: null }
         ]
     },
     {
         id: 8,
-        score: 21,
+        score: 20,
         holes: [
 
-            { side: "top", start: 300, end: 600, color: null, closesAt: null },
-            { side: "bottom", start: 100, end: 300, color: null, closesAt: null },
-            { side: "left", start: 100, end: 300, color: "gold", closesAt: 10 },
-            { side: "right", start: 300, end: 600, color: "gold", closesAt: 10 }
+            { side: "top", start: 0, end: 200, color: "dodgerblue", closesAt: 8 },
+            { side: "top", start: 400, end: 600, color: "dodgerblue", closesAt: 8 },
+            { side: "bottom", start: 200, end: 400, color: "dodgerblue", closesAt: 8 },
+            { side: "left", start: 200, end: 400, color: "gold", closesAt: 3 },
+            { side: "right", start: 0, end: 200, color: "gold", closesAt: 3 },
+            { side: "right", start: 400, end: 600, color: "gold", closesAt: 3 }
         ]
     },
     {
         id: 9,
         score: 23,
         holes: [
-            { side: "top", start: 0, end: 350, color: "purple", closesAt: 11 },
-            { side: "bottom", start: 250, end: 600, color: "purple", closesAt: 11 },
-            { side: "left", start: 250, end: 600, color: "purple", closesAt: 11 },
-            { side: "right", start: 0, end: 350, color: "purple", closesAt: 11 }
+            { side: "top", start: 0, end: 100, color: "purple", closesAt: 3 },
+            { side: "top", start: 250, end: 350, color: "purple", closesAt: 3 },
+            { side: "bottom", start: 250, end: 350, color: "purple", closesAt: 3 },
+            { side: "bottom", start: 500, end: 600, color: "purple", closesAt: 3 },
+            { side: "left", start: 250, end: 350, color: "purple", closesAt: 3 },
+            { side: "left", start: 500, end: 600, color: "purple", closesAt: 3 },
+            { side: "right", start: 0, end: 100, color: "purple", closesAt: 3 },
+            { side: "right", start: 250, end: 350, color: "purple", closesAt: 3 }
         ]
     },
     {
         id: 10,
         score: 26,
         holes: [
-            { side: "top", start: 150, end: 450, color: "red", closesAt: 15 },
-            { side: "bottom", start: 150, end: 450, color: "red", closesAt: 15 },
-            { side: "left", start: 150, end: 450, color: "red", closesAt: 15 },
-            { side: "right", start: 150, end: 450, color: "red", closesAt: 15 },
+            { side: "top", start: 150, end: 250, color: "red", closesAt: 3 },
+            { side: "top", start: 350, end: 450, color: "red", closesAt: 3 },
+            { side: "bottom", start: 150, end: 250, color: "red", closesAt: 3 },
+            { side: "bottom", start: 350, end: 450, color: "red", closesAt: 3 },
+            { side: "left", start: 150, end: 250, color: "red", closesAt: 3 },
+            { side: "left", start: 350, end: 450, color: "red", closesAt: 3 },
+            { side: "right", start: 150, end: 250, color: "red", closesAt: 3 },
+            { side: "right", start: 350, end: 450, color: "red", closesAt: 3 },
 
             { side: "top", start: 0, end: 50, color: "hotpink", closesAt: 8 },
             { side: "top", start: 550, end: 600, color: "hotpink", closesAt: 8 },
